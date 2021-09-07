ca1460671a2667d61c746d1fd2c7b9cf641cbe25
renho@nas-centos7 <rho_i_xx@163.com>
2021-09-07 13:35:25 +0000

    jdk8_test

--- java/jdk8/src/main/java/com/renho/jdk8/function/Car.java
+++ java/jdk8/src/main/java/com/renho/jdk8/function/Car.java
@@ -6,29 +6,29 @@ import java.util.function.Supplier;
 
 public class Car {
 
-	public static Car create(final Supplier<Car> supplier) {
-		return supplier.get();
-	}
+    public static Car create(final Supplier<Car> supplier) {
+        return supplier.get();
+    }
 
-	public static void collide(final Car car) {
-		System.out.println("Collided " + car.toString());
-	}
+    public static void collide(final Car car) {
+        System.out.println("Collided " + car.toString());
+    }
 
-	public void follow(final Car another) {
-		System.out.println("Following the " + another.toString());
-	}
+    public void follow(final Car another) {
+        System.out.println("Following the " + another.toString());
+    }
 
-	public void repair() {
-		System.out.println("Repaired " + this.toString());
-	}
+    public void repair() {
+        System.out.println("Repaired " + this.toString());
+    }
 
-	public static void main(String[] args) {
-		final Car car = Car.create(Car::new);
-		final List<Car> cars = Arrays.asList(car);
-		
-		cars.forEach(Car::collide);
-		cars.forEach(Car::repair);
-		final Car police = Car.create(Car::new);
-		cars.forEach(police::follow);
-	}
+    public static void main(String[] args) {
+        final Car car = Car.create(Car::new);
+        final List<Car> cars = Arrays.asList(car);
+
+        cars.forEach(Car::collide);
+        cars.forEach(Car::repair);
+        final Car police = Car.create(Car::new);
+        cars.forEach(police::follow);
+    }
 }
--- java/jdk8/src/main/java/com/renho/jdk8/lambda/Main.java
+++ java/jdk8/src/main/java/com/renho/jdk8/lambda/Main.java
@@ -4,34 +4,39 @@ import java.util.Arrays;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Arrays.asList("a", "b", "c").forEach(e -> System.out.println(e));
+    public static void main(String[] args) {
+        Arrays.asList("a", "b", "c").forEach(e -> System.out.println(e));
 
-		Arrays.asList("a", "b", "c").forEach(
-				(String e) -> System.out.println(e));
+        Arrays.asList("a", "b", "c").forEach(
+                (String e) -> System.out.println(e));
 
-		Arrays.asList("a", "b", "d").forEach(e -> {
-			System.out.print(e);
-			System.out.print(e);
-		});
+        Arrays.asList("a", "b", "d").forEach(e -> {
+            System.out.print(e);
+            System.out.print(e);
+        });
 
-		String separator = ",";
-		Arrays.asList("a", "b", "d").forEach(
-				(String e) -> System.out.print(e + separator));
+        String separator = ",";
+        Arrays.asList("a", "b", "d").forEach(
+                (String e) -> System.out.print(e + separator));
 
-		Arrays.asList("a", "b", "d").sort((e1, e2) -> {
-			int result = e1.compareTo(e2);
-			return result;
-		});
-		System.out.println();
-		FunctionalDefaultMethods fdm = new FunctionalDefaultMethods() {
-			
-			@Override
-			public void method() {
-				System.out.println(11111);
-			}
-		};
-		fdm.defaultMethod();
-	}
+        Arrays.asList("a", "b", "d").sort((e1, e2) -> {
+            int result = e1.compareTo(e2);
+            return result;
+        });
+        System.out.println();
+        FunctionalDefaultMethods fdm = new FunctionalDefaultMethods() {
+
+            @Override
+            public void method() {
+                System.out.println(11111);
+            }
+        };
+        fdm.defaultMethod();
+
+        fdm = () -> {
+            System.out.println("() -> ");
+        };
+        fdm.method();
+    }
 
 }
